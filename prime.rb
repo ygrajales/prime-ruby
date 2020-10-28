d123ab12da5a57311d7ca141647e9f52645e50bc
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-10-28 16:17:56 -0500

    commit tree d08b0685b8d670833073bc71388f217c1ce02ee4

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+
+    range = (2..number-1).to_a
+
+    if number <= 1
+        false
+    elsif number == 2
+        true
+    elsif range.any?{|factor| number % factor == 0}
+        false
+    else
+        true
+    end
+
+end
+
